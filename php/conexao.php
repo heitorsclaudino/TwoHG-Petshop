8cf11b2d449e6e65c47533a3902301a72c4d12db
Heitor <heitorsclaudino04@gmail.com>
2023-06-18 19:45:19 -0300

    Update conexao.php

--- php/conexao.php
+++ php/conexao.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = '';
-    $dbname = 'twohg';
+    $servername = 'sql305.infinityfree.com';
+    $username = 'if0_34434310';
+    $password = 'Araqa6JKaMyEPI';
+    $dbname = 'if0_34434310_twohg';
 
     $conn = mysqli_connect($servername, $username, $password, $dbname) or die('A conexão com o servidor falhou!');
 ?>
